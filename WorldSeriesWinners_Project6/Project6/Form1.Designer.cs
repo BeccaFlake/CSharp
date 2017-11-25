929ed48d5df5b2babddc0fdc0fe0ef7c5c4733b2
Rebecca <31871431+BeccaFlake@users.noreply.github.com>
2017-11-25 15:29:07 -0500

    Add files via upload

---
+++ WorldSeriesWinners_Project6/Project6/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Project6
+{
+    partial class worldSeriesWinners
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.teamsListBox = new System.Windows.Forms.ListBox();
+            this.instructionLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // teamsListBox
+            // 
+            this.teamsListBox.FormattingEnabled = true;
+            this.teamsListBox.Location = new System.Drawing.Point(27, 56);
+            this.teamsListBox.Name = "teamsListBox";
+            this.teamsListBox.Size = new System.Drawing.Size(165, 251);
+            this.teamsListBox.TabIndex = 1;
+            this.teamsListBox.SelectedIndexChanged += new System.EventHandler(this.teamsListBox_SelectedIndexChanged);
+            // 
+            // instructionLabel
+            // 
+            this.instructionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.instructionLabel.Location = new System.Drawing.Point(45, 15);
+            this.instructionLabel.Name = "instructionLabel";
+            this.instructionLabel.Size = new System.Drawing.Size(296, 30);
+            this.instructionLabel.TabIndex = 0;
+            this.instructionLabel.Text = "Choose a team from the list to see the number of times they have won the World Se" +
+    "ries";
+            this.instructionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(209, 140);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(161, 112);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "The [team name] have won the World Series [number] time(s).";
+            // 
+            // exitButton
+            // 
+            this.exitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exitButton.Location = new System.Drawing.Point(171, 313);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // worldSeriesWinners
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exitButton;
+            this.ClientSize = new System.Drawing.Size(408, 349);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.instructionLabel);
+            this.Controls.Add(this.teamsListBox);
+            this.Name = "worldSeriesWinners";
+            this.Text = "World Series Winners";
+            this.Load += new System.EventHandler(this.worldSeriesWinners_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox teamsListBox;
+        private System.Windows.Forms.Label instructionLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ WorldSeriesWinners_Project6/Project6/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Project6
+{
+    public partial class worldSeriesWinners : Form
+    {
+        public worldSeriesWinners()
+        {
+            InitializeComponent();
+        }
+        //Declare global variables
+        const int SIZE = 107;                       //the world series was played 107 times between 1903 and 2012. 
+        string[] worldSeries = new string[SIZE];    //array to hold the winning team from each year
+
+
+        /*** Function that handles the file tasks when the form loads***/
+       private void worldSeriesWinners_Load(object sender, EventArgs e)
+        {
+            readToListBox();
+            readToArray(worldSeries);
+        }
+
+        /*** Function that reads the contents of the Teams file into the ListBox Control***/
+        private void readToListBox()
+        {
+            //Try to open the file and read the contents
+            try
+            {
+                //Declare StreamReader Object for teams
+                StreamReader teamsFile;
+                //Open the file and get the object
+                teamsFile = File.OpenText("../../Teams.txt");
+
+                string winningTeam;         //variable to hold the team
+
+                while (!teamsFile.EndOfStream)
+                {
+                    //Get the team
+                    winningTeam = teamsFile.ReadLine();
+                    //Add the team to the list box
+                    teamsListBox.Items.Add(winningTeam);
+                }
+                //Close the file
+                teamsFile.Close();
+            }
+            //Catch and handle an exception.
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        /***Function that reads the contents of the World Series file into an array ***/
+        private void readToArray(string[] worldSeries)
+        {  
+            //Try to open the file and read the contents
+            try {
+                 //Declare StreamReader Object for world series
+                    StreamReader worldSeriesFile;
+                    //Open the file and get the object
+                    worldSeriesFile = File.OpenText("../../WorldSeries.txt");
+                    string worldSeriesYear;         //variable to hold the world series year
+
+                    //Read the file
+                    while (!worldSeriesFile.EndOfStream)
+                    {   //Step through the array and add the winning team from that year to it
+                        for (int i = 0; i < 107; i++)
+                        {   //Read the line from the file into the variable
+                            worldSeriesYear = worldSeriesFile.ReadLine();
+                            //Set the element to the contents of the variable
+                            worldSeries[i] = worldSeriesYear;
+                        }
+                    }
+                //Close the file
+                worldSeriesFile.Close();
+                }
+            //Catch and handle an exception.
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message); }
+        }
+
+        /***Function that searches the array of winners and returns the count***/
+        private int arraySearch(string team, int count)
+        { 
+            //Step through the array
+            for (int i = 0; i < SIZE; i++)
+            {   
+                //If the name of the selected team matches the contents at the array index
+                if (team == worldSeries[i])
+                { count++;}            //add one to the counter  
+            }
+            return count;
+        }
+
+        /***Event handler for the ListBox Control***/
+        private void teamsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int count = 0;                              //variable to hold the number of times the selected team has won
+            string team = "";                           //varaible to hold the team selected by the user
+
+            //Get the selected item
+            team = teamsListBox.SelectedItem.ToString();
+            //Call the arraySearch function to get the count
+            count = arraySearch(team, count);
+            //Display the results in the label
+            resultLabel.Text = "The " + team + " have won the World Series " + count.ToString() + " time(s).";
+        }
+        /***Event Handler for the exit button***/
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
